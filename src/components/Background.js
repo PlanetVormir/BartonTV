3571251222a85e2dd68a3db0cf51ae5415a5a421
Sujal Singh <email.sujalsingh@gmail.com>
2022-10-26 19:56:56 +0530

    new home screen

--- src/components/Background.js
+++ src/components/Background.js
@@ -3,28 +3,23 @@ import {StyleSheet, View} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
 const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-  },
   background: {
     flex: 1,
   },
   container: {
     flex: 1,
-    flexDirection: 'column',
     padding: '4%',
   },
 });
 
 const Background = props => {
   return (
-    <View style={styles.body}>
-      <LinearGradient
-        style={styles.background}
-        colors={['rgba(17,26,33, 0.6)', 'rgb(17,26,33)']}>
-        <View style={styles.container}>{props.children}</View>
-      </LinearGradient>
-    </View>
+    <LinearGradient
+      style={styles.background}
+      colors={['rgb(22,27,34)', 'rgb(13,17,23)']}
+      locations={[0.3, 1]}>
+      <View style={styles.container}>{props.children}</View>
+    </LinearGradient>
   );
 };
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,33 +1,41 @@
 import React, {useCallback, useState} from 'react';
 import {
   Dimensions,
+  TouchableOpacity,
   ImageBackground,
-  TouchableHighlight,
   StyleSheet,
 } from 'react-native';
 
+import LinearGradient from 'react-native-linear-gradient';
+
 const styles = StyleSheet.create({
-  wrapper: {
-    backgroundColor: 'slategray',
+  gradient: {
     width: Dimensions.get('window').width * 0.28,
     aspectRatio: 16 / 9,
     margin: 25,
     borderRadius: 20,
+  },
+  wrapper: {
+    flex: 1,
+    backgroundColor: '#2E303A',
     overflow: 'hidden',
+    borderRadius: 18,
+    borderColor: 'rgba(0,0,0,0)',
   },
   wrapperFocused: {
-    borderWidth: 5,
-    borderColor: 'lavender',
-    elevation: 70,
+    borderWidth: 6,
+    borderRadius: 20,
   },
   image: {
-    width: '100%',
-    height: '100%',
+    width: '105%',
+    height: '105%',
     backgroundColor: 'rgba(0,0,0,0)',
   },
 });
 
 const Card = props => {
+  styles.gradient.aspectRatio = props.vertical ? 9 / 16 : 16 / 9;
+
   const [hasFocus, setFocus] = useState(false);
 
   const onFocus = useCallback(() => {
@@ -46,17 +54,25 @@ const Card = props => {
   }, []);
 
   return (
-    <TouchableHighlight
-      hasTVPreferredFocus={props.defaultFocus}
-      onFocus={onFocus}
-      onPress={onPress}
-      onBlur={onBlur}
-      style={[styles.wrapper, hasFocus ? styles.wrapperFocused : null]}>
-      <ImageBackground
-        style={styles.image}
-        source={{uri: props.data ? props.data.poster : null}}
-      />
-    </TouchableHighlight>
+    <LinearGradient
+      style={styles.gradient}
+      start={{x: 1, y: 1}}
+      end={{x: 0, y: 0}}
+      locations={[0.12, 0.42, 0.84]}
+      colors={['#d2a8ff', '#f778ba', '#ff7b72']}>
+      <TouchableOpacity
+        style={[styles.wrapper, hasFocus ? styles.wrapperFocused : null]}
+        hasTVPreferredFocus={props.defaultFocus}
+        onFocus={onFocus}
+        onPress={onPress}
+        onBlur={onBlur}
+        activeOpacity={1}>
+        <ImageBackground
+          style={styles.image}
+          source={{uri: props.data ? props.data.poster : null}}
+        />
+      </TouchableOpacity>
+    </LinearGradient>
   );
 };
 
